ea48e5a6164c7f639e20b599d10fad22f5dad1ae
user <user@402-18>
2016-10-27 16:41:08 +0800

    10/28 First Commit and Push to GitHub.

---
+++ src/com/alaya/TestUnit1.java
@@ -0,0 +1,14 @@
+package com.alaya;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestUnit1 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
